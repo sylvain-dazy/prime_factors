c37d111f4570104a2af8514cf2e07b89c6810e05
Sylvain Dazy <sylvain.dazy@hotmail.com>
2023-10-11 09:55:05 +0200

    Creating an application

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from prime_factors import prime_factors_of
+
 if __name__ == '__main__':
-    print("Hello world!")
+    print("Enter a number less than 1 in order to close the application")
+    n = int(input("What number do you want to decomposed ? "))
+    while n > 1:
+        print(f"The decomposition of {n} is {prime_factors_of(n)}")
+        n = int(input("What number do you want to decomposed ? "))
