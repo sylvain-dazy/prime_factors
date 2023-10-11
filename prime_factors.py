55194991c65a7ee3628d5685705ab49efdd9318c
Sylvain Dazy <sylvain.dazy@hotmail.com>
2023-10-11 09:07:19 +0200

    prime factors of 2 is 2 prime factors of 3 is 3

--- prime_factors.py
+++ prime_factors.py
@@ -1,6 +1,11 @@
 def prime_factors_of(n: int) -> list:
-    return []
+    factors = []
+    if n > 1:
+        factors.append(n)
+    return factors
 
 
 def test_prime_factors():
     assert prime_factors_of(1) == []
+    assert prime_factors_of(2) == [2]
+    assert prime_factors_of(3) == [3]
