3c29bc912347afa13889546c71f586df8ed26eb2
Sylvain Dazy <sylvain.dazy@hotmail.com>
2023-10-11 09:15:20 +0200

    prime factors of 5 is [5] prime factors of 6 is [2, 3] prime factors of 7 is [7]

--- prime_factors.py
+++ prime_factors.py
@@ -14,3 +14,6 @@ def test_prime_factors():
     assert prime_factors_of(2) == [2]
     assert prime_factors_of(3) == [3]
     assert prime_factors_of(4) == [2, 2]
+    assert prime_factors_of(5) == [5]
+    assert prime_factors_of(6) == [2, 3]
+    assert prime_factors_of(7) == [7]
