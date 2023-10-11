383ae6c813d71442b9340338cdc63b53263394ac
Sylvain Dazy <sylvain.dazy@hotmail.com>
2023-10-11 09:10:26 +0200

    prime factors of 4 is [2, 2]

--- prime_factors.py
+++ prime_factors.py
@@ -1,5 +1,9 @@
-def prime_factors_of(n: int) -> list:
+def prime_factors_of(n: int) -> list[int]:
     factors = []
+    if n > 1:
+        if n % 2 == 0:
+            factors.append(2)
+            n /= 2
     if n > 1:
         factors.append(n)
     return factors
@@ -9,3 +13,4 @@ def test_prime_factors():
     assert prime_factors_of(1) == []
     assert prime_factors_of(2) == [2]
     assert prime_factors_of(3) == [3]
+    assert prime_factors_of(4) == [2, 2]
